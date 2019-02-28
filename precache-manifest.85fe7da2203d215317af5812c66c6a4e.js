6802e34a7115c3ee3cffc0080a11a7b0fee4e285
graphicskart <graphicskart2018@gmail.com>
2019-02-28 09:21:24 +0530

    Updates

---
+++ precache-manifest.85fe7da2203d215317af5812c66c6a4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06bb8b44c86951225d61",
+    "url": "/upload-form/static/js/runtime~main.06bb8b44.js"
+  },
+  {
+    "revision": "222b114395f360b4f357",
+    "url": "/upload-form/static/js/main.222b1143.chunk.js"
+  },
+  {
+    "revision": "46f7615cc55815d6def3",
+    "url": "/upload-form/static/js/2.46f7615c.chunk.js"
+  },
+  {
+    "revision": "222b114395f360b4f357",
+    "url": "/upload-form/static/css/main.5d2581cb.chunk.css"
+  },
+  {
+    "revision": "57e8896b629449ca6441ed0c36595166",
+    "url": "/upload-form/index.html"
+  }
+];
